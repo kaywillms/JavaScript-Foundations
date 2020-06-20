49cd8b37119bda79801c92909ae20479525a7a97
kaywillms <53545248+kaywillms@users.noreply.github.com>
2020-06-19 22:18:17 -0400

    updated

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
-*/
+ */
 
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
 
 
 
@@ -15,6 +17,8 @@
 (2) Create another variable called `periods` and give it the value of years*12.
 */
 
+const monthlyInterestRate = interestRate / 12;
+const periods = years * 12;
 
 
 
@@ -35,6 +39,11 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
+let n1 = (1 + monthlyInterestRate) ^ periods;
+let numerator = principal * n1 * monthlyInterestRate;
+let denominator = n1 - 1;
+let monthlyRate = numerator / denominator;
+
 
 
 
@@ -44,7 +53,25 @@ When your math is correct, monthlyRate will equal 1073.64
 If your name is `Oscar` mortgageCalculator() should return "Oscar, your monthly rate is 1073.64"
 */
 
+function mortgageCalculator() {
+ let principal = 200000;
+ let interestRate = 0.05;
+ let years = 30;
+ let name = "Kayan"
 
+
+ let monthlyInterestRate = interestRate / 12;
+ let periods = years * 12;
+
+
+ let n1 = (1 + monthlyInterestRate) ^ periods;
+ let numerator = principal * n1 * monthlyInterestRate;
+ let denominator = n1 - 1;
+ let monthlyRate = numerator / denominator;
+
+ return `name, your monthly rate is ${monthlyRate}`;
+}
+mortgageCalculator()
 
 
 
@@ -55,7 +82,21 @@ For example,
 mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 */
 
+function mortgageCalculator(P, I, N) {
 
+ let monthlyInterestRate = I / 12;
+ let periods = N * 12;
+
+
+ let n1 = (1 + monthlyInterestRate) ^ periods;
+ let numerator = P * n1 * monthlyInterestRate;
+ let denominator = n1 - 1;
+ let monthlyRate = numerator / denominator;
+
+ return Math.round(monthlyRate);
+}
+
+mortgageCalculator(200000, 0.05, 30)
 
 
 
